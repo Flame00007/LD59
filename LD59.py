67305b75c0dd44c37287d177c1972b4875ad533a
Gwenael Hubart <gwenael.hubart@gmail.com>
2026-04-20 22:36:36 +0200

    mise a jour

--- LD59.py
+++ LD59.py
@@ -49,8 +49,19 @@ class dentifrice():
         self.degree = 0
         self.en_cours = False
         self.tailleX, self.tailleY = ecran.get_size()
+        self.directXreturn = 0
+        self.directYreturn = -1
+        self.directX = 1
+        self.directY = 0
     def createtir(self, moveX, moveY):
-        self.liste.append(goutte(moveX, moveY, self.x + 130, self.y + 300, self.degree + 90))
+        if self.degree == 0:
+            self.liste.append(goutte(moveX, moveY, self.x + 170, self.y , self.degree + 90))
+        if self.degree == 90:
+            self.liste.append(goutte(moveX, moveY, self.x, self.y + 130, self.degree + 90))
+        if self.degree == 180 :
+            self.liste.append(goutte(moveX, moveY, self.x + 130, self.y + 300, self.degree + 90))
+        if self.degree == 270:
+            self.liste.append(goutte(moveX, moveY, self.x + 300, self.y + 130, self.degree + 90))
     def tir(self, dent):
         suppr = []
         for i in range(len(tube.liste)):
@@ -66,13 +77,20 @@ class dentifrice():
             return True
         return False
     def offense(self, perso, tailleX):
-        x = perso.x - 300
-        x2 = perso.x + 300
-        if 0.185*tailleX > perso.x - 300 :
+        x = perso.x - 200
+        x2 = perso.x + 200
+        y = perso.y - 200
+        y2 = perso.y + 200
+        if 0.185*tailleX > perso.x - 200 :
             x = 0.185*tailleX
-        if tailleX - 0.185*tailleX < perso.x + 300 :
+        if tailleX - 0.185*tailleX < perso.x + 200 :
             x2 = tailleX - 0.185*tailleX
-        self.cible = random.randint(int(x), int(x2))
+        if 0.180*tailleY > perso.y - 200 :
+            y = 0.180*tailleY
+        if tailleY - 0.180*tailleY < perso.y + 200 :
+            y2 = tailleY - 0.180*tailleY
+        self.cibleX = random.randint(int(x), int(x2))
+        self.cibleY = random.randint(int(y), int(y2))
         self.en_cours = True
     def tp(self, x, y):
         self.x = x
@@ -80,6 +98,31 @@ class dentifrice():
     def rotate(self, degree):
         self.degree = degree
         self.image = pygame.transform.rotate(self.image, self.degree)
+    def changerotate(self):
+        self.image = pygame.image.load('Signal3b.png')
+        self.image = pygame.transform.scale(self.image, (300, 400))
+        self.degree = 0
+        a = random.randint(0,3)
+        if a == 0:
+            self.rotate(0)
+            self.tp((self.tailleX - 300) / 2, self.tailleY )
+            self.directXreturn, self.directYreturn = 0, 1
+            self.directX, self.directY = 1, 0
+        if a == 1:
+            self.rotate(270)
+            self.tp(- 400, (self.tailleY - 300) / 2)
+            self.directXreturn, self.directYreturn = -1, 0
+            self.directX, self.directY = 0, 1
+        if a == 2:
+            self.rotate(180)
+            self.tp((tailleX - 300)/2, - 400)
+            self.directXreturn, self.directYreturn = 0, -1
+            self.directX, self.directY = 1, 0
+        if a == 3:
+            self.rotate(90)
+            self.tp(self.tailleX , (self.tailleY - 300 ) / 2)
+            self.directXreturn, self.directYreturn = 1, 0
+            self.directX, self.directY = 0, 1
     def move(self, x, y):
         self.x = self.x + x
         self.y = self.y + y
@@ -98,21 +141,16 @@ class dent():
     def tp(self, x, y):
         self.x = x
         self.y = y
-    def touch(self, x, y, tailleX, tailleY):
-        if x < self.x + 50 < x + tailleX and y < self.y < y + tailleY:
-            return True
-        if x < self.x < x + tailleX and y < self.y + 100 < y + tailleY:
-            return True
-        if x < self.x < x + tailleX and y < self.y < y + tailleY:
-            return True
-        if x < self.x + 100 < x + tailleX and y < self.y < y + tailleY:
-            return True
-        if x < self.x + 100 < x + tailleX and y < self.y + 100 < y + tailleY:
+    def touch(self, x, y, tailleX1, tailleY1):
+        zone_rect1 = pygame.Rect((x, y), (tailleX1, tailleY1))
+        zone_rect2 = pygame.Rect((self.x, self.y), (100, 100))
+        if zone_rect1.colliderect(zone_rect2):
             return True
         return False
     def move(self, x, y):
         self.x = self.x + x
         self.y = self.y + y
+        tailleX, tailleY = ecran.get_size()
         if self.y > tailleY - 0.24*tailleY:
             self.y = tailleY - 0.24*tailleY
         if self.y < 0.05*tailleY + 100:
@@ -174,23 +212,53 @@ score1 = time.perf_counter()
 
 myfont = pygame.font.SysFont("monospace", 50)
 
+tempschange1 = time.perf_counter()
+
+changeAller = False
+
+changeRetour = False
+
+compteur = 0
+
 var = True
 
 
 
 while var == True:
-    score2 = time.perf_counter()
+    tempschange2 = time.perf_counter()
+    if delai(tempschange1, tempschange2, 5):
+        tempschange1 = time.perf_counter()
+        changeAller = True
+        compteur = 0
+    if changeAller == True:
+        if compteur == 200:
+            tube.changerotate()
+            changeAller = False
+            changeRetour = True
+            compteur = 0
+        tube.move(tube.directXreturn, tube.directYreturn)
+        compteur += 1
+    if changeRetour == True:
+        if compteur == 200:
+            changeRetour = False
+        tube.move(-tube.directXreturn, -tube.directYreturn)
+        compteur += 1
+    if changeAller == False and changeRetour == False:
+        score2 = time.perf_counter()
     ecart, vitesse, vitesseBalle = lv(score1, score2)
     ecran.blit(ring, (0, 0))
     phrase = f'{int(score2 - score1)}'
     score_display = myfont.render(phrase, 1, (255, 255, 0))
     ecran.blit(score_display, (0, 0))
     perso.show()
-    if tube.tir(perso) == True:
-        perso.etat -= 1
-        if perso.etat < 0:
-            var = False
-        perso.choice(perso.etat)
+    for i in range (len(tube.liste)):
+        a = perso.touch(tube.liste[i].x, tube.liste[i].y, 75, 50)
+        if a == True :
+            perso.etat -= 1
+            if perso.etat < 0:
+                var = False
+            perso.choice(perso.etat)
+    tube.tir(perso)
     tube.show()
     time2 = time.perf_counter()
     if delai(time1, time2, ecart):
@@ -198,14 +266,22 @@ while var == True:
         time1 = time.perf_counter()
         tube.en_cours = True
     if tube.en_cours == True:
-        if tube.cible-10 < tube.x < tube.cible+10:
-            tube.createtir(0, vitesseBalle)
+        if tube.cibleX-10 < tube.x + 150 < tube.cibleX+10:
+            tube.createtir(-tube.directXreturn * vitesseBalle, -tube.directYreturn * vitesseBalle)
             tube.en_cours = False
-        if tube.cible < tube.x:
-            tube.move(-vitesse, 0)
-        if tube.cible > tube.x:
-            tube.move(vitesse, 0)
-
+        if tube.degree == 0 or tube.degree == 180:
+            if tube.cibleX < tube.x + 150:
+                tube.move(-vitesse, 0)
+            if tube.cibleX > tube.x + 150 :
+                tube.move(vitesse, 0)
+        if tube.degree == 90 or tube.degree == 270:
+            if tube.cibleY < tube.y + 150:
+                tube.move(0, -vitesse)
+            if tube.cibleY > tube.y + 150:
+                tube.move(0, vitesse)
+        if tube.cibleY-10 < tube.y + 150 < tube.cibleY+10:
+            tube.createtir(-tube.directXreturn * vitesseBalle, -tube.directYreturn * vitesseBalle)
+            tube.en_cours = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             var = False
@@ -251,3 +327,6 @@ while var == True:
     ecran.blit(ring, (0, 0))
     ecran.blit(score_display, ((tailleX - 100)/2, (tailleY - 300)/2))
     pygame.display.flip()
+
+
+
